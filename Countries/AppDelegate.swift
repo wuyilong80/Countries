8ce42adfbab6e4e142011c39388dbd7fc69f05b0
LawrenceWu <lawrence.wu@tpisoftware.com>
2023-03-08 14:32:49 +0800

    first init

---
+++ Countries/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Countries
+//
+//  Created by Lawrence on 2023/3/8.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+    
+}
+
